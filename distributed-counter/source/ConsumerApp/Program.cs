1fde94b0623ba489b6f10378d1ca273753d36814
Sandeep Nair <nair.sandeep@microsoft.com>
2023-04-24 17:50:23 +0530

    initial commit

---
+++ distributed-counter/source/ConsumerApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.Net;
+using System.Collections;
+using System.Drawing;
+using System.Diagnostics.Metrics;
+using CosmosDistributedCounter;
+using DistributedCounterConsumerApp;
+
+namespace Cosmos_Patterns_DistributedCounter
+{
+    internal class Program
+    {
+        private static DistributedCounterOperationalService dcos;
+        private static PrimaryCounter pc;
+        private static object _lock;
+
+        static async Task Main(string[] args)
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
+
+            var config = configuration.Build();
+
+            string endpoint = config["CosmosUri"];
+            string key = config["CosmosKey"];
+            string databaseName = config["CosmosDatabase"];
+            string containerName = config["CosmosContainer"];
+
+            dcos = new DistributedCounterOperationalService(endpoint, key, databaseName, containerName);
+
+            await MainAsync();
+
+          
+        }
+
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("Running Distributed Counter Consumer ...");
+
+
+            while(pc == null)
+            {
+                Console.WriteLine("Enter the Counter ID");
+                string counterId = Console.ReadLine().Trim();
+
+                Console.WriteLine("Getting Counter...");
+                pc = await dcos.GetPrimaryCounterAsync(counterId);
+                if (pc == null)
+                {
+                    Console.WriteLine("Primary Counter couldn't be found. Press any key to retry.");
+                    Console.ReadLine();
+                }
+            }
+
+            int threadCount = 2;
+            Console.WriteLine("Enter the number of worker threads required");
+            int.TryParse(Console.ReadLine().Trim(), out threadCount);
+
+            _lock = new object();
+            for (int i=0;i<threadCount;i++)
+            {
+                WorkerThread wt = new WorkerThread(pc, dcos, new PostMessageCallback(MessageCallback));
+                wt.StartThread();
+            }
+
+            Console.WriteLine(threadCount + " worker threads are running... ,hit any key to exit");
+            var input = Console.ReadLine();
+        }
+
+
+
+        public static void MessageCallback(ConsoleMessage msg)
+        {
+
+            lock (_lock)
+            {
+
+                Console.ForegroundColor = msg.Color;
+                Console.WriteLine(msg.Message);
+
+            }
+        }
+
+    }
+}
---
+++ distributed-counter/source/ConsumerApp/WorkerThread.cs
@@ -0,0 +1,72 @@
+﻿using CosmosDistributedCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedCounterConsumerApp
+{
+    public record ConsoleMessage(
+           string Message,
+           ConsoleColor Color
+       );
+
+    // Delegate that defines the signature for the callback method.
+    public delegate void PostMessageCallback(ConsoleMessage msg);
+
+    internal class WorkerThread
+    {
+        PrimaryCounter pc;
+        DistributedCounterOperationalService dcos;
+        public bool isActive = true;
+        PostMessageCallback postMessage;
+        public WorkerThread(PrimaryCounter _pc, DistributedCounterOperationalService _dcos, PostMessageCallback _postMessage)
+        {
+            this.pc = _pc;
+            this.dcos = _dcos;
+            this.postMessage = _postMessage;
+        }
+
+
+        public async void StartThread()
+        {            
+            while (this.isActive)
+            {
+                //pick a random number to decrement
+                Random r = new Random();
+                int decrementVal = r.Next(1, 4);
+
+                try
+                {
+                    if (await dcos.DecrementDistributedCounterValueAsync(pc, decrementVal) == false)
+                    {
+                        postMessage(new ConsoleMessage($"Warning: Decrement Failed", ConsoleColor.Yellow));
+                    }
+                    else
+                    {
+                        postMessage(new ConsoleMessage($"Sucess: Decrement by "+ decrementVal, ConsoleColor.Blue));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    postMessage(new ConsoleMessage($"Exception: " + ex.Message, ConsoleColor.Red));
+                }
+
+                //DO WORK, delay before next execution
+                await DoWork();
+            }
+        }
+
+        private async Task DoWork()
+        {
+            //wait some random time
+            Random r = new Random();
+            int delay = r.Next(250, 500);
+           
+            await Task.Delay(delay);
+        }
+    }
+}
